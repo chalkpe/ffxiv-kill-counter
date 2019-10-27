070711d2abc12309091f937c5587c72bb1c7d89d
Chalk <chalkpe@gmail.com>
2019-10-27 13:01:44 +0900

    first implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span v-if="!me">플레이어 인식 불가</span>
+    <table>
+      <tr v-for="entry in counts" :key="entry[0]">
+        <td>{{ entry[0] }}</td><td>{{ entry[1] }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import listen from './listener'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  data: () => ({ me: 0, counter: {}, ban: [] }),
+
+  mounted () {
+    listen(d => {
+      const m = `on${d.type.substr(0, 1).toUpperCase()}${d.type.substr(1)}`
+      if (typeof this[m] === 'function') try { this[m](d) } catch (e) { console.error(e) }
+    })
+  },
+
+  computed: {
+    counts () {
+      const entries = Object.entries(this.counter)
+      entries.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
+
+      return entries
+    }
+  },
+
+  methods: {
+    onMe ({ id }) {
+      this.me = id
+    },
+
+    onCmd ({ args }) {
+      args = args.trim().split(' ')
+
+      if (args[0] === 'reset') {
+        this.counter = {}
+      }
+
+      if (args[0] === 'ban' && args.length > 1) {
+        const name = args.slice(1).join(' ').trim()
+
+        this.$delete(this.counter, name)
+        if (this.ban.indexOf(name) < 0) this.ban.push(name)
+      }
+    },
+
+    onKill ({ attacker, victim }) {
+      if (attacker.id !== this.me) return
+      if (this.ban.indexOf(victim.name) > -1) return
+
+      const count = this.counter[victim.name] || 0
+      this.$set(this.counter, victim.name, count + 1)
+    }
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  font-size: 14px;
+  line-height: 1rem;
+  letter-spacing: -0.1rem;
+  text-shadow: 0 0 3px #ffc72f;
+  font-family: '맑은 고딕', sans-serif;
+}
+
+tr td:first-child {
+  text-align: right;
+  padding-right: 0.3rem;
+}
+
+tr td:last-child {
+  text-align: left;
 }
 </style>
